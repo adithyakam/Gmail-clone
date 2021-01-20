7d45dae10d52449a4a468305f44982e525c3aaaf
adithyakam <50355901+adithyakam@users.noreply.github.com>
2021-01-20 07:43:54 +0530

    updating sidebar

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import SideBarOption from './SideBarOption';
+
+
 
 import {Button} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import NoteIcon from '@material-ui/icons/Note';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 
@@ -11,6 +20,9 @@ function SideBar() {
     return (
         <div className='sidebar'>
             <Button startIcon={<AddIcon fontSize='Large' />  } className='sidebar__compose'>Compose</Button>
+            <SideBarOption Icon={InboxIcon} title='inbox'  number={54}  selected={true}/>
+            <SideBarOption Icon={StarIcon} title='inbox'  number={54}/>
+
         </div>
     )
 }
